9fb8984faf427ee86e00325504a7c2e4e0092292
Alex Gusev <alex@flancer64.com>
2017-12-29 15:04:16 +0200

    MOBI-1066 Create referral bonus module

---
+++ Config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Module's configuration (hard-coded).
+ *
+ * User: Alex Gusev <alex@flancer64.com>
+ */
+
+namespace Praxigento\BonusReferral;
+
+class Config
+    extends \Praxigento\BonusBase\Config
+{
+    const MODULE = 'Praxigento_BonusReferral';
+}
