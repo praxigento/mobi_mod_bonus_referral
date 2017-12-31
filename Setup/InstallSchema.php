32a962c455a5a1914ed20ee1a3319bae706fe265
Alex Gusev <alex@flancer64.com>
2017-12-31 21:59:19 +0200

    MOBI-1073 Clean up InstallScheme classes in modules

--- Setup/InstallSchema.php
+++ Setup/InstallSchema.php
@@ -9,22 +9,19 @@ namespace Praxigento\BonusReferral\Setup;
 
 use Praxigento\BonusReferral\Repo\Entity\Data\Registry as Registry;
 
-/**
- * @SuppressWarnings(PHPMD.CamelCaseMethodName)
- */
 class InstallSchema
     extends \Praxigento\Core\App\Setup\Schema\Base
 {
 
-    protected function _setup()
+    protected function setup()
     {
         /** Read and parse JSON schema. */
         $pathToFile = __DIR__ . '/../etc/dem.json';
         $pathToNode = '/dBEAR/package/Praxigento/package/Bonus/package/Referral';
-        $demPackage = $this->_toolDem->readDemPackage($pathToFile, $pathToNode);
+        $demPackage = $this->toolDem->readDemPackage($pathToFile, $pathToNode);
 
         /* Registry */
         $demEntity = $demPackage->get('entity/Registry');
-        $this->_toolDem->createEntity(Registry::ENTITY_NAME, $demEntity);
+        $this->toolDem->createEntity(Registry::ENTITY_NAME, $demEntity);
     }
 }
