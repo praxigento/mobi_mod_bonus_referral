3d3a51982b07ce7accf336fea44a57dbb3a8ab3c
Alex Gusev <alex@flancer64.com>
2018-03-23 19:25:25 +0200

    MOBI-1205 Warehouse currency for referral bonus

--- Service/Sale/Register.php
+++ Service/Sale/Register.php
@@ -17,35 +17,56 @@ use Praxigento\BonusReferral\Service\Sale\Register\Response as AResponse;
  */
 class Register
 {
+    /** @var \Praxigento\Downline\Repo\Dao\Customer */
+    private $daoDwnl;
+    /** @var \Praxigento\BonusReferral\Repo\Dao\Registry */
+    private $daoReg;
+    /** @var \Magento\Directory\Model\CurrencyFactory */
+    private $factCur;
     /** @var \Praxigento\BonusReferral\Helper\Config */
     private $hlpConfig;
+    /** @var \Praxigento\Warehouse\Api\Helper\Stock */
+    private $hlpStock;
     /** @var \Praxigento\Core\Api\App\Logger\Main */
     private $logger;
-    /** @var \Praxigento\Downline\Repo\Dao\Customer */
-    private $repoDwnl;
-    /** @var \Praxigento\BonusReferral\Repo\Dao\Registry */
-    private $repoReg;
+    /** @var \Magento\Framework\App\Config\ScopeConfigInterface */
+    private $scopeConfig;
     /** @var \Praxigento\BonusReferral\Service\Sale\Calc */
     private $servCalc;
 
     public function __construct(
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Magento\Directory\Model\CurrencyFactory $factCur,
         \Praxigento\Core\Api\App\Logger\Main $logger,
-        \Praxigento\Downline\Repo\Dao\Customer $repoDwnl,
-        \Praxigento\BonusReferral\Repo\Dao\Registry $repoReg,
+        \Praxigento\Downline\Repo\Dao\Customer $daoDwnl,
+        \Praxigento\BonusReferral\Repo\Dao\Registry $daoReg,
         \Praxigento\BonusReferral\Helper\Config $hlpConfig,
+        \Praxigento\Warehouse\Api\Helper\Stock $hlpStock,
         \Praxigento\BonusReferral\Service\Sale\Calc $servCalc
     ) {
+        $this->scopeConfig = $scopeConfig;
+        $this->factCur = $factCur;
         $this->logger = $logger;
-        $this->repoDwnl = $repoDwnl;
-        $this->repoReg = $repoReg;
+        $this->daoDwnl = $daoDwnl;
+        $this->daoReg = $daoReg;
         $this->hlpConfig = $hlpConfig;
+        $this->hlpStock = $hlpStock;
         $this->servCalc = $servCalc;
     }
 
+    /**
+     * Calculate bonus bounty & fee and convert to the base currency (for WALLET asset).
+     *
+     * @param \Magento\Sales\Model\Order $sale
+     * @param int $uplineId
+     * @return array
+     * @throws \Exception
+     */
     private function calcAmounts(
         \Magento\Sales\Model\Order $sale,
         int $uplineId
     ) {
+        /* call to service to calculate bonus */
         $req = new ACalcReq();
         $req->setSaleOrder($sale);
         $req->setUplineId($uplineId);
@@ -53,7 +74,18 @@ class Register
         $resp = $this->servCalc->exec($req);
         $amount = $resp->getDelta();
         $fee = $resp->getFee();
-        return [$amount, $fee];
+        /* convert warehouse base currency into system base currency */
+        $curBase = $this->scopeConfig->getValue(\Magento\Directory\Model\Currency::XML_PATH_CURRENCY_BASE);
+        $storeId = $sale->getStoreId();
+        $curWrhs = $this->hlpStock->getStockCurrencyByStoreId($storeId);
+        $curr = $this->factCur->create();
+        $curr->load($curWrhs);
+        $amountBase = $curr->convert($amount, $curBase);
+        $amountBase = round($amountBase, 2);
+        $feeBase = $curr->convert($fee, $curBase);
+        $feeBase = round($feeBase, 2);
+
+        return [$amountBase, $feeBase];
     }
 
     public function exec($request)
@@ -91,7 +123,7 @@ class Register
     private function getUplineId($custId)
     {
         /** @var \Praxigento\Downline\Repo\Data\Customer $entity */
-        $entity = $this->repoDwnl->getById($custId);
+        $entity = $this->daoDwnl->getById($custId);
         $result = $entity->getParentId();
         return $result;
     }
@@ -102,7 +134,8 @@ class Register
      * @param int $saleId
      * @param int $custId
      * @param float $amount
-     * @param floet $fee
+     * @param float $fee
+     * @param string $state
      */
     private function registerBonus($saleId, $custId, $amount, $fee, $state)
     {
@@ -112,6 +145,6 @@ class Register
         $entity->setAmountTotal($amount);
         $entity->setAmountFee($fee);
         $entity->setState($state);
-        $this->repoReg->create($entity);
+        $this->daoReg->create($entity);
     }
 }
