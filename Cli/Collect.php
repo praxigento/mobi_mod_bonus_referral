a23dbf8a03a2ef6a10b2582f8679246af4b72179
Alex Gusev <alex@flancer64.com>
2019-04-05 15:04:15 +0300

    SAN-614 Investigate bonus calcs on test

--- Cli/Collect.php
+++ Cli/Collect.php
@@ -13,44 +13,22 @@ use Praxigento\BonusReferral\Api\Service\Bonus\Collect\Request as ARequest;
 class Collect
     extends \Praxigento\Core\App\Cli\Cmd\Base
 {
-    /** @var \Magento\Framework\DB\Adapter\AdapterInterface */
-    private $conn;
-    /** @var \Magento\Framework\App\ResourceConnection */
-    private $resource;
     /** @var \Praxigento\BonusReferral\Service\Bonus\Collect */
     private $servCollect;
 
     public function __construct(
-        \Magento\Framework\ObjectManagerInterface $manObj,
-        \Magento\Framework\App\ResourceConnection $resource,
         \Praxigento\BonusReferral\Service\Bonus\Collect $servCollect
     ) {
         parent::__construct(
-            $manObj,
             'prxgt:bonus:referral:collect',
             'Collect and pay referral bonus after delay.'
         );
-        $this->resource = $resource;
-        $this->conn = $resource->getConnection();
         $this->servCollect = $servCollect;
     }
 
-    protected function execute(
-        \Symfony\Component\Console\Input\InputInterface $input,
-        \Symfony\Component\Console\Output\OutputInterface $output
-    ) {
-        $output->writeln("<info>Start referral bonus collection.<info>");
-        /* wrap all DB operations with DB transaction */
-        $this->conn->beginTransaction();
-        try {
-            $req = new ARequest();
-            $this->servCollect->exec($req);
-            $this->conn->commit();
-        } catch (\Throwable $e) {
-            $output->writeln('<info>Command \'' . $this->getName() . '\' failed. Reason: '
-                . $e->getMessage() . '.<info>');
-            $this->conn->rollBack();
-        }
-        $output->writeln('<info>Command \'' . $this->getName() . '\' is completed.<info>');
+    protected function process(\Symfony\Component\Console\Input\InputInterface $input)
+    {
+        $req = new ARequest();
+        $this->servCollect->exec($req);
     }
 }
